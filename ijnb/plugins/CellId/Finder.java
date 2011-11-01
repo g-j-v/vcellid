fcd0130c865abb29e4d6bf0bed727958d536ec72
petit.ale <petit.ale@f3c6994c-f1c5-1524-d009-d389855cafb4>
2011-11-01 00:28:54 +0000

    Actualizado para devolver un JTree

--- ijnb/plugins/CellId/Finder.java
+++ ijnb/plugins/CellId/Finder.java
@@ -6,10 +6,13 @@
 package CellId;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 /**
  *
@@ -21,22 +24,24 @@ public class Finder {
 
     File directory;
     String separator;
+    List<String> fileNames;
 
-    public Finder(File directory, String separator){
+    public Finder(File directory, String separator , List<String> patterns){
+        //Indicar el separador de tiempo y posicion como variable.
         setDirectory(directory);
         this.separator = separator;
+        this.fileNames = new ArrayList<String>();
+        find(patterns);
     }
 
-    public Finder(File directory){
+    public Finder(File directory,List<String> patterns){
 
         // The default separator is the lower dash "_"
-        this(directory,"_");
+        this(directory,"_",patterns);
     }
 
     //recieves the patterns in the order they should appear
-    public List<String> find(List<String> patterns){
-
-        List<String> founded = new ArrayList<String>();
+    private void find(List<String> patterns){
 
         String pattern = "";
         for(String str : patterns){
@@ -48,12 +53,78 @@ public class Finder {
         for(String childs : directory.list()){
             Matcher matcher = pat.matcher(childs);
             if(matcher.matches()){
-                founded.add(matcher.group());
+                fileNames.add(matcher.group());
             }
 
         }
-        return founded;
     }
+
+    public JTree generateTree(List<String> fileNames){
+        JTree tree;
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode("Images");
+    createNodes(top);
+    tree = new JTree(top);
+        return tree;
+    }
+
+    public void createNodes(DefaultMutableTreeNode top){
+
+        int maxPositions = getMaxPosition();
+        for(int i = 0; i < maxPositions; ++i ){
+            Position position = new Position(i);
+            DefaultMutableTreeNode positionNode = new DefaultMutableTreeNode(position);
+            int maxTimesForPosition = getMaxTime(position);
+            for(int j = 0; j < maxTimesForPosition; ++j){
+                Time time = new Time(position,j);
+                DefaultMutableTreeNode timeNode = new DefaultMutableTreeNode(time);
+                File[] files = directory.listFiles(new NameFilter(position,time));
+                time.setFiles(files);
+                for(File f : files){
+                    timeNode.add(new DefaultMutableTreeNode(f.getName()));
+                }
+                positionNode.add(timeNode);
+            }
+            top.add(positionNode);
+        }
+    }
+
+    public int getMaxPosition(){
+
+       //TODO: Para generalizar todavia mas podemos recibir un parametro
+       // que identifique el nombre con el que empieza la posicion, o guardarlo
+       // como variable de instancia de Position
+        int max = 0;
+        for(String name: fileNames){
+            int aux;
+            int pos = name.toLowerCase().indexOf("position");
+            aux = Integer.valueOf(name.substring(pos+8,pos+9));
+            if(aux > max){
+                max = aux;
+            }
+        }
+        return max;
+    }
+
+    public int getMaxTime(Position position){
+
+       //TODO: Para generalizar todavia mas podemos recibir un parametro
+       // que identifique el nombre con el que empieza el tiempo, o guardarlo
+       // como variable de instancia de Time
+       int max = 0;
+        for(String name: fileNames){
+            int aux;
+            if(name.toLowerCase().contains(position.toString())){
+                int pos = name.toLowerCase().indexOf("time_");
+                aux = Integer.valueOf(name.substring(pos+5,pos+8));
+                if(aux > max){
+                    max = aux;
+                }
+
+            }
+        }
+        return max;
+    }
+
 
     public void setDirectory(File file){
         if(!file.isDirectory()){
@@ -61,5 +132,26 @@ public class Finder {
         }else{
             this.directory = file;
         }
+    }
+    private class NameFilter implements FilenameFilter{
+
+        Position position;
+        Time time;
+
+        private NameFilter(Position position, Time time){
+
+            this.position = position;
+            this.time = time;
+        }
+
+        public boolean accept(File dir, String name) {
+           if(name.toLowerCase().contains(position.toSrting()) &&
+                   name.toLowerCase().contains(time.toString())){
+               return true;
+           }else{
+               return false;
+           }
+        }
+
     }
 }
