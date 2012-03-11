80d5c05dd57ea93ef8daf7a9f742d33321e1f92d
gisela.delavilla@gmail.com <gisela.delavilla@gmail.com@f3c6994c-f1c5-1524-d009-d389855cafb4>
2012-03-11 01:25:32 +0000

    File chooser paraa seleccionar directorio de las imágenes para el cellId

--- trunk/IJ/src/CellId/LoadImages_.java
+++ trunk/IJ/src/CellId/LoadImages_.java
@@ -5,91 +5,96 @@
 
 package CellId;
 
+import ij.io.OpenDialog;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JPanel;
-import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.JFileChooser;
 
 import utils.Finder;
 
-
 /**
- *
+ * 
  * @author alejandropetit
  */
-public class LoadImages_ extends ij.plugin.frame.PlugInFrame{
-    
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTree jTree1;
+public class LoadImages_ extends ij.plugin.frame.PlugInFrame {
 
-    public LoadImages_(){
-        super("LoadImages");
-        
-        
+	private javax.swing.JScrollPane jScrollPane1;
+	private javax.swing.JTree jTree1;
 
-    }
+	public LoadImages_() {
+		super("LoadImages");
 
-    public void run(String arg){
-    	
+	}
 
-        List<String> patterns = new ArrayList<String>();
-        //Pattern para los fields
-        patterns.add("BF|CFP|YFP");
-        //Pattern para la posicion
-        patterns.add("Position\\d{1,}");
-        //Pattern para el tiempo
-        patterns.add("time_\\d{3}\\.tif");
-        Finder finder = new Finder(new File("/Volumes/APETIT/2004-11-11-TCY3154-inhibitor-effect/"),patterns);
+	public void run(String arg) {
 
-        jTree1 = finder.generateTree();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        
-        jTree1.setName("jTree1"); // NOI18N
-        jScrollPane1.setName("jScrollPane1"); // NOI18N
+		JFileChooser fc = new JFileChooser();
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int returnVal = fc.showOpenDialog(LoadImages_.this);
+		if (returnVal != JFileChooser.APPROVE_OPTION) {
+			return;
+		}
 
+		File file = fc.getSelectedFile();
 
+		List<String> patterns = new ArrayList<String>();
+		// Pattern para los fields
+		patterns.add("BF|CFP|YFP");
+		// Pattern para la posicion
+		patterns.add("Position\\d{1,}");
+		// Pattern para el tiempo
+		patterns.add("time_\\d{3}\\.tif");
+		
+		Finder finder = new Finder(file, patterns);
+//		Finder finder = new Finder(new File(
+//				"/Volumes/APETIT/2004-11-11-TCY3154-inhibitor-effect/"),
+//				patterns);
 
-        jScrollPane1.setViewportView(jTree1);
+		jTree1 = finder.generateTree();
+		jScrollPane1 = new javax.swing.JScrollPane();
 
-       add(jScrollPane1);
-       setSize(1000,600);
-       show();
-    }
-    /**
-    private void createNodes(DefaultMutableTreeNode top) {
+		jTree1.setName("jTree1"); // NOI18N
+		jScrollPane1.setName("jScrollPane1"); // NOI18N
 
-        DefaultMutableTreeNode category = null;
-        DefaultMutableTreeNode book = null;
+		jScrollPane1.setViewportView(jTree1);
 
-        category = new DefaultMutableTreeNode("Books for Java Programmers");
-        top.add(category);
-
-        //original Tutorial
-        book = new DefaultMutableTreeNode("The Java Tutorial: A Short Course on the Basics");
-        category.add(book);
-
-        //Tutorial Continued
-        book = new DefaultMutableTreeNode("The Java Tutorial Continued: The Rest of the JDK");
-        category.add(book);
-
-        //JFC Swing Tutorial
-        book = new DefaultMutableTreeNode("The JFC Swing Tutorial: A Guide to Constructing GUIs");
-        category.add(book);
-
-        //...add more books for programmers...
-
-        category = new DefaultMutableTreeNode("Books for Java Implementers");
-        top.add(category);
-
-        //VM
-        book = new DefaultMutableTreeNode("The Java Virtual Machine Specification");
-        category.add(book);
-
-        //Language Spec
-        book = new DefaultMutableTreeNode("The Java Language Specification");
-        category.add(book);
-    }
-    */
+		add(jScrollPane1);
+		setSize(1000, 600);
+		show();
+	}
+	/**
+	 * private void createNodes(DefaultMutableTreeNode top) {
+	 * 
+	 * DefaultMutableTreeNode category = null; DefaultMutableTreeNode book =
+	 * null;
+	 * 
+	 * category = new DefaultMutableTreeNode("Books for Java Programmers");
+	 * top.add(category);
+	 * 
+	 * //original Tutorial book = new DefaultMutableTreeNode(
+	 * "The Java Tutorial: A Short Course on the Basics"); category.add(book);
+	 * 
+	 * //Tutorial Continued book = new DefaultMutableTreeNode(
+	 * "The Java Tutorial Continued: The Rest of the JDK"); category.add(book);
+	 * 
+	 * //JFC Swing Tutorial book = new DefaultMutableTreeNode(
+	 * "The JFC Swing Tutorial: A Guide to Constructing GUIs");
+	 * category.add(book);
+	 * 
+	 * //...add more books for programmers...
+	 * 
+	 * category = new DefaultMutableTreeNode("Books for Java Implementers");
+	 * top.add(category);
+	 * 
+	 * //VM book = new
+	 * DefaultMutableTreeNode("The Java Virtual Machine Specification");
+	 * category.add(book);
+	 * 
+	 * //Language Spec book = new
+	 * DefaultMutableTreeNode("The Java Language Specification");
+	 * category.add(book); }
+	 */
 }
