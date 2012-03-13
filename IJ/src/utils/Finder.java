5f34d044a2e957400762d5ef63876ceaa3578976
petit.ale <petit.ale@f3c6994c-f1c5-1524-d009-d389855cafb4>
2012-03-13 20:58:52 +0000

    Separe el armado del arbol del buscador de archivos. Class TreeGenerator added. Hice los cambios en loadImages, etc. No llegue a probar por que no tengo las imagenes. Estoy en proceso de des-hard-code-ear algunas cosas.

--- IJ/src/utils/Finder.java
+++ IJ/src/utils/Finder.java
@@ -31,27 +31,27 @@ public class Finder {
 
     //storing the Directory to look for files and the patterns separator
 
-    File directory;
     String separator;
-    List<String> fileNames;
+    List<String> patterns;
 
-    public Finder(File directory, String separator , List<String> patterns){
+
+    public Finder(String separator , List<String> patterns){
         //Indicar el separador de tiempo y posicion como variable.
-        setDirectory(directory);
         this.separator = separator;
-        this.fileNames = new ArrayList<String>();
-        find(patterns);
+        this.patterns = patterns;
     }
 
-    public Finder(File directory,List<String> patterns){
+    public Finder(List<String> patterns){
 
         // The default separator is the lower dash "_"
-        this(directory,"_",patterns);
+        this("_",patterns);
     }
 
-    //recieves the patterns in the order they should appear
-    private void find(List<String> patterns){
+    //recieves the directory where to look files, and returns a list of files that match.
+    public List<String> find(File directory){
 
+    	List<String> fileNames = new ArrayList<String>();
+    	
         String pattern = "";
         for(String str : patterns){
             pattern += str;
@@ -66,129 +66,8 @@ public class Finder {
             }
 
         }
+        
+        return fileNames;
     }
 
-    public JTree generateTree(){
-        final JTree tree;
-        DefaultMutableTreeNode top = new DefaultMutableTreeNode("Images");
-    createNodes(top);
-    tree = new JTree(top);
-    tree.addTreeSelectionListener(new TreeSelectionListener() {
-		
-		@Override
-		public void valueChanged(TreeSelectionEvent e) {
-			DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-			System.out.println("Evento");
-			if(node == null || node.getChildCount() > 0){
-				System.out.println("hola");
-				return;
-			}
-			
-			String filePath = node.getUserObject().toString();
-			System.out.println(directory.getAbsolutePath());
-			System.out.println(filePath);
-			IJ.open(directory.getAbsolutePath() + "/" +filePath);				
-			
-		}
-	});
-        return tree;
-    }
-
-    public void createNodes(DefaultMutableTreeNode top){
-        System.out.println("TAM: " + fileNames.size());
-        int maxPositions = getMaxPosition();
-        for(int i = 1; i <= maxPositions; ++i ){
-            Position position = new Position(i);
-            //Adding position to root
-            DefaultMutableTreeNode positionNode = new DefaultMutableTreeNode(position);
-            top.add(positionNode);
-            int maxTimesForPosition = getMaxTime(position);
-            for(int j = 1; j <= maxTimesForPosition; ++j){
-                Time time = new Time(position,j);
-                DefaultMutableTreeNode timeNode = new DefaultMutableTreeNode(time);
-                //Adding Time node to position
-                positionNode.add(timeNode);
-                File[] files = directory.listFiles(new NameFilter(position,time));
-                time.setFiles(files);
-                for(File f : files){
-                    timeNode.add(new DefaultMutableTreeNode(f.getName()));
-                }
-                
-            }
-            
-        }
-    }
-
-    public int getMaxPosition(){
-
-       //TODO: Para generalizar todavia mas podemos recibir un parametro
-       // que identifique el nombre con el que empieza la posicion, o guardarlo
-       // como variable de instancia de Position
-        int max = 0;
-        for(String name: fileNames){
-            int aux;
-            int startPos = name.toLowerCase().indexOf("position");
-            int endPos = name.toLowerCase().indexOf("_time");
-            aux = Integer.valueOf(name.substring(startPos+8,endPos));
-            if(aux > max){
-                max = aux;
-            }
-        }
-        System.out.println("Max Pos: " + max);
-        return max;
-    }
-
-    public int getMaxTime(Position position){
-
-       //TODO: Para generalizar todavia mas podemos recibir un parametro
-       // que identifique el nombre con el que empieza el tiempo, o guardarlo
-       // como variable de instancia de Time
-       int max = 0;
-       for(String name: fileNames){
-            int aux;
-            
-            if(name.toLowerCase().contains(position.toString())){
-                int startPos = name.toLowerCase().indexOf("time_");
-                int endPos = name.toLowerCase().indexOf(".tif");
-                aux = Integer.valueOf(name.substring(startPos+5,endPos));
-//                System.err.println("Num de time" + name.substring(startPos+5,endPos));
-                if(aux > max){
-                    max = aux;
-                }
-
-            }
-        }
-//        System.out.println("Max time:" + max);
-        return max;
-    }
-
-
-    public void setDirectory(File file){
-        if(!file.isDirectory()){
-            throw new IllegalArgumentException("Path is not a directory");
-        }else{
-            this.directory = file;
-        }
-    }
-    private class NameFilter implements FilenameFilter{
-
-        Position position;
-        Time time;
-
-        private NameFilter(Position position, Time time){
-
-            this.position = position;
-            this.time = time;
-        }
-
-        public boolean accept(File dir, String name) {
-           if(name.toLowerCase().contains(position.toString()) &&
-                   name.toLowerCase().contains(time.toString())){
-               return true;
-           }else{
-               return false;
-           }
-        }
-
-    }
 }
---
+++ IJ/src/utils/TreeGenerator.java
@@ -0,0 +1,152 @@
+package utils;
+
+import ij.IJ;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.List;
+
+import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class TreeGenerator {
+	
+	private Finder finder;
+	private File directory;
+	private List<String> fileNames;	
+	
+	public TreeGenerator(Finder finder, File directory){
+		this.finder = finder;
+		this.directory = directory;
+		this.fileNames = finder.find(directory);
+	}
+	
+	public JTree generateTree(){
+        final JTree tree;
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode("Images");
+    createNodes(top,directory);
+    tree = new JTree(top);
+    tree.addTreeSelectionListener(new TreeSelectionListener() {
+		
+		@Override
+		public void valueChanged(TreeSelectionEvent e) {
+			DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+			System.out.println("Evento");
+			if(node == null || node.getChildCount() > 0){
+				System.out.println("hola");
+				return;
+			}
+			
+			String filePath = node.getUserObject().toString();
+			System.out.println(directory.getAbsolutePath());
+			System.out.println(filePath);
+			IJ.open(directory.getAbsolutePath() + "/" +filePath);				
+			
+		}
+	});
+        return tree;
+    }
+
+    public void createNodes(DefaultMutableTreeNode top, File directory){
+    	
+    	List<String> fileNames = finder.find(directory);
+    	
+        System.out.println("TAM: " + fileNames.size());
+        int maxPositions = getMaxPosition();
+        for(int i = 1; i <= maxPositions; ++i ){
+            Position position = new Position(i);
+            //Adding position to root
+            DefaultMutableTreeNode positionNode = new DefaultMutableTreeNode(position);
+            top.add(positionNode);
+            int maxTimesForPosition = getMaxTime(position);
+            for(int j = 1; j <= maxTimesForPosition; ++j){
+                Time time = new Time(position,j);
+                DefaultMutableTreeNode timeNode = new DefaultMutableTreeNode(time);
+                //Adding Time node to position
+                positionNode.add(timeNode);
+                File[] files = directory.listFiles(new NameFilter(position,time));
+                time.setFiles(files);
+                for(File f : files){
+                    timeNode.add(new DefaultMutableTreeNode(f.getName()));
+                }
+                
+            }
+            
+        }
+    }
+
+    public int getMaxPosition(){
+
+       //TODO: Para generalizar todavia mas podemos recibir un parametro
+       // que identifique el nombre con el que empieza la posicion, o guardarlo
+       // como variable de instancia de Position
+        int max = 0;
+        for(String name: fileNames){
+            int aux;
+            // Busco el segundo separador para ver el identificador de la posicion
+            int endPos = name.toLowerCase().indexOf(finder.separator,name.indexOf(finder.separator + 1));
+            int i;
+            for(i = endPos - 1; name.charAt(i) >= '0' && name.charAt(i) <= '9' ; i--){
+            	;
+            }
+            aux = Integer.valueOf(name.substring(i,endPos));
+            if(aux > max){
+                max = aux;
+            }
+        }
+        System.out.println("Max Pos: " + max);
+        return max;
+    }
+
+    public int getMaxTime(Position position){
+
+       //TODO: Para generalizar todavia mas podemos recibir un parametro
+       // que identifique el nombre con el que empieza el tiempo, o guardarlo
+       // como variable de instancia de Time
+       int max = 0;
+       for(String name: fileNames){
+            int aux;
+            
+            if(name.toLowerCase().contains(position.toString())){
+                int endPos = name.toLowerCase().indexOf(".");
+                int i;
+                for(i = endPos - 1; name.charAt(i) >= '0' && name.charAt(i) <= '9' ; i--){
+                	;
+                }
+                aux = Integer.valueOf(name.substring(i,endPos));
+//                System.err.println("Num de time" + name.substring(startPos+5,endPos));
+                if(aux > max){
+                    max = aux;
+                }
+
+            }
+        }
+//        System.out.println("Max time:" + max);
+        return max;
+    }
+
+    private class NameFilter implements FilenameFilter{
+
+        Position position;
+        Time time;
+
+        private NameFilter(Position position, Time time){
+
+            this.position = position;
+            this.time = time;
+        }
+
+        public boolean accept(File dir, String name) {
+           if(name.toLowerCase().contains(position.toString()) &&
+                   name.toLowerCase().contains(time.toString())){
+               return true;
+           }else{
+               return false;
+           }
+        }
+
+    }
+    
+}
