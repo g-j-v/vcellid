7d14e5e5d46088d5e7260f0d5de04d6347ac7bf6
petit.ale <petit.ale@f3c6994c-f1c5-1524-d009-d389855cafb4>
2012-03-23 05:45:17 +0000

    Botones agregados para el arbol en loadImages. Selecciona lo indicado, pero no lo pinta, tengo que ver eso.

--- IJ/src/CellId/LoadImages.java
+++ IJ/src/CellId/LoadImages.java
@@ -1,18 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package CellId;
 
-import ij.io.OpenDialog;
+import ij.IJ;
+import ij.gui.ImageCanvas;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.MediaTracker;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -23,12 +16,16 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
 
 import panels.PicturePanel;
-
 import utils.Finder;
 import utils.TreeGenerator;
+
+
 
 /**
  * 
@@ -91,7 +88,7 @@ public class LoadImages extends ij.plugin.frame.PlugInFrame implements ActionLis
 			return;
 		}
 
-		File file = fc.getSelectedFile();
+		final File file = fc.getSelectedFile();
 
 		List<String> patterns = new ArrayList<String>();
 		// Pattern para los fields
@@ -108,7 +105,7 @@ public class LoadImages extends ij.plugin.frame.PlugInFrame implements ActionLis
 		
 		picturePanel = new PicturePanel(null);
 	    // Preferred height is irrelevant, since using WEST region
-		picturePanel.setPreferredSize(new Dimension(400, 0));
+		picturePanel.setPreferredSize(new Dimension(600, 600));
 		picturePanel.setBorder(BorderFactory.createLineBorder (Color.blue, 2));
 		picturePanel.setBackground(Color.white);
 	    add(picturePanel, BorderLayout.EAST);
@@ -116,6 +113,7 @@ public class LoadImages extends ij.plugin.frame.PlugInFrame implements ActionLis
 		TreeGenerator treeGenerator = new TreeGenerator(finder, file);
 		
 		jTree1 = treeGenerator.generateTree(picturePanel);
+		jTree1.setExpandsSelectedPaths(true);
 		jScrollPane1 = new javax.swing.JScrollPane();
 
 		jTree1.setName("jTree1"); // NOI18N
@@ -131,12 +129,170 @@ public class LoadImages extends ij.plugin.frame.PlugInFrame implements ActionLis
 	    buttonsPanel.setPreferredSize(new Dimension(800, 100));
 	    buttonsPanel.setBorder(BorderFactory.createLineBorder (Color.blue, 2));
 	    buttonsPanel.setBackground(Color.white);
-	    buttonsPanel.add(new JButton("Previous"));
-	    buttonsPanel.add(new JButton("Next"));
+
+	    JButton previousPositionButton = new JButton("Previous Position");
+	    previousPositionButton.setPreferredSize(new Dimension(150,20));
+	    previousPositionButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(jTree1.getLastSelectedPathComponent() != null){
+					DefaultMutableTreeNode node = (DefaultMutableTreeNode) jTree1.getLastSelectedPathComponent();
+					DefaultMutableTreeNode previousNode;
+					if(node.isLeaf()){
+						DefaultMutableTreeNode positionParent = (DefaultMutableTreeNode) node.getParent().getParent();
+						previousNode = positionParent.getPreviousSibling();
+					}else if(node.getUserObject().toString().toLowerCase().contains("time")){
+						DefaultMutableTreeNode positionParent = (DefaultMutableTreeNode) node.getParent();
+						previousNode = positionParent.getPreviousSibling();
+					}else if(node.getUserObject().toString().toLowerCase().contains("position")){
+						previousNode = node.getPreviousSibling();
+					}else{
+						return;
+					}
+					if(previousNode != null ){
+						jTree1.setSelectionPath(new TreePath(previousNode));
+						System.out.println(file.getAbsolutePath() + "/" +previousNode.getUserObject().toString());
+					}
+				}
+			}
+		});
+	    buttonsPanel.add(previousPositionButton);
+	    
+	    JButton previousTimeButton = new JButton("Previous Time");
+	    previousTimeButton.setPreferredSize(new Dimension(150,20));
+	    previousTimeButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(jTree1.getLastSelectedPathComponent() != null){
+					DefaultMutableTreeNode node = (DefaultMutableTreeNode) jTree1.getLastSelectedPathComponent();
+					DefaultMutableTreeNode previousNode;
+					if(node.isLeaf()){
+						DefaultMutableTreeNode parent = (DefaultMutableTreeNode)node.getParent();
+						previousNode = parent.getPreviousSibling();
+					}else if(node.getUserObject().toString().toLowerCase().contains("time")){
+						previousNode = node.getPreviousSibling();
+					}else if(node.getUserObject().toString().toLowerCase().contains("position")){
+						previousNode = (DefaultMutableTreeNode) node.getLastChild();
+					}else{
+						return;
+					}
+					if(previousNode != null){
+						jTree1.setSelectionPath(new TreePath(previousNode));
+						System.out.println(file.getAbsolutePath() + "/" +previousNode.getUserObject().toString());
+					}
+				}
+			}
+		});
+	    buttonsPanel.add(previousTimeButton);
+	    
+	    JButton previousButton = new JButton("Previous Image");
+	    previousButton.setPreferredSize(new Dimension(150,20));
+	    previousButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(jTree1.getLastSelectedPathComponent() != null){
+					DefaultMutableTreeNode node = (DefaultMutableTreeNode) jTree1.getLastSelectedPathComponent();
+					DefaultMutableTreeNode previousNode = node.getPreviousSibling();
+					if(previousNode != null && previousNode.isLeaf()){
+						TreePath path = new TreePath(previousNode);
+						jTree1.setSelectionPath(path);
+						jTree1.scrollPathToVisible(path);
+						System.out.println(file.getAbsolutePath() + "/" +previousNode.getUserObject().toString());
+						picturePanel.setImage(new ImageCanvas(IJ.openImage(file.getAbsolutePath() + "/" +previousNode.getUserObject().toString())));
+						picturePanel.getImage().paint(picturePanel.getGraphics());
+					}
+				}
+			}
+		});
+	    buttonsPanel.add(previousButton);
+	    
+	    JButton nextButton = new JButton("Next Image");
+	    nextButton.setPreferredSize(new Dimension(150,20));
+	    nextButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(jTree1.getLastSelectedPathComponent() != null){
+					DefaultMutableTreeNode node = (DefaultMutableTreeNode) jTree1.getLastSelectedPathComponent();
+					DefaultMutableTreeNode nextNode = node.getNextSibling();
+					if(nextNode != null && nextNode.isLeaf()){
+						TreePath path = new TreePath(nextNode);
+						jTree1.setSelectionPath(path);
+						jTree1.scrollPathToVisible(path);
+						System.out.println(file.getAbsolutePath() + "/" +nextNode.getUserObject().toString());
+						picturePanel.setImage(new ImageCanvas(IJ.openImage(file.getAbsolutePath() + "/" + nextNode.getUserObject().toString())));
+						picturePanel.getImage().paint(picturePanel.getGraphics());
+					}
+				}
+			}
+		});
+	    buttonsPanel.add(nextButton);
+
+	    JButton nextTimeButton = new JButton("Next Time");
+	    nextTimeButton.setPreferredSize(new Dimension(150,20));
+	    nextTimeButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(jTree1.getLastSelectedPathComponent() != null){
+					DefaultMutableTreeNode node = (DefaultMutableTreeNode) jTree1.getLastSelectedPathComponent();
+					DefaultMutableTreeNode nextNode;
+					if(node.isLeaf()){
+						DefaultMutableTreeNode parent = (DefaultMutableTreeNode)node.getParent();
+						nextNode = parent.getNextSibling();
+					}else if(node.getUserObject().toString().toLowerCase().contains("time")){
+						nextNode = node.getNextSibling();
+					}else if(node.getUserObject().toString().toLowerCase().contains("position")){
+						nextNode = (DefaultMutableTreeNode) node.getFirstChild();
+					}else{
+						return;
+					}
+					if(nextNode != null){
+						jTree1.setSelectionPath(new TreePath(nextNode));
+						System.out.println(file.getAbsolutePath() + "/" +nextNode.getUserObject().toString());
+					}
+				}
+			}
+		});
+	    buttonsPanel.add(nextTimeButton);
+	    
+	    JButton nextPositionButton = new JButton("Next Position");
+	    nextPositionButton.setPreferredSize(new Dimension(150,20));
+	    nextPositionButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(jTree1.getLastSelectedPathComponent() != null){
+					DefaultMutableTreeNode node = (DefaultMutableTreeNode) jTree1.getLastSelectedPathComponent();
+					DefaultMutableTreeNode nextNode;
+					if(node.isLeaf()){
+						DefaultMutableTreeNode positionParent = (DefaultMutableTreeNode) node.getParent().getParent();
+						nextNode = positionParent.getNextSibling();
+					}else if(node.getUserObject().toString().toLowerCase().contains("time")){
+						DefaultMutableTreeNode positionParent = (DefaultMutableTreeNode) node.getParent();
+						nextNode = positionParent.getNextSibling();
+					}else if(node.getUserObject().toString().toLowerCase().contains("position")){
+						nextNode = node.getNextSibling();
+					}else{
+						return;
+					}
+					if(nextNode != null){
+						jTree1.setSelectionPath(new TreePath(nextNode));
+						System.out.println(file.getAbsolutePath() + "/" +nextNode.getUserObject().toString());
+					}
+				}
+			}
+		});
+	    buttonsPanel.add(nextPositionButton);
+	    
 	    add(buttonsPanel, BorderLayout.SOUTH);
 	    
 	    
-		setSize(800, 500);
+		setSize(1000, 700);
+		setResizable(false);
 		show();
 	}
 }
