3f98bf6d5ae52cddefc24c48377c7b1d7bb1cc48
petit.ale <petit.ale@f3c6994c-f1c5-1524-d009-d389855cafb4>
2012-11-17 20:14:16 +0000

    Made ErrorWindow extend from PlugInFrame

--- trunk/IJ/src/cellid/error/ErrorWindow.java
+++ trunk/IJ/src/cellid/error/ErrorWindow.java
@@ -1,5 +1,7 @@
 package cellid.error;
 
+import ij.plugin.frame.PlugInFrame;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -12,7 +14,7 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import javax.swing.SwingConstants;
 
-public class ErrorWindow extends JFrame {
+public class ErrorWindow extends PlugInFrame {
 
 	private JPanel contentPane;
 	
@@ -20,19 +22,21 @@ public class ErrorWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public ErrorWindow(String message) {
+		
+		super("Error");
 
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 342, 186);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+//		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblErrorMessage = new JLabel(message);
 		lblErrorMessage.setHorizontalAlignment(SwingConstants.CENTER);
 		lblErrorMessage.setBounds(45, 31, 239, 84);
 		contentPane.add(lblErrorMessage);
-		
-		this.setVisible(true);
+		add(contentPane);
+		setVisible(true);
 	}
 }
